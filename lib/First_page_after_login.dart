8f97feab0f61eb725c03c011bc0ee06794115d83
Praveen Dilshan <rosapraveen1@gmail.com>
2024-05-26 21:13:47 +0530

    First page after login

---
+++ lib/First_page_after_login.dart
@@ -0,0 +1,153 @@
+import 'package:eco_bin_original/Location_add_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FirstPageAfterLogin());
+}
+
+class FirstPageAfterLogin extends StatelessWidget {
+  const FirstPageAfterLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Center(
+            child: Text(
+              "ECOBIN",
+              style: TextStyle(
+                color: Color.fromARGB(255, 9, 94, 2),
+                fontSize: 55,
+                fontFamily: 'Jaro',
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(
+                  top: 45,
+                ),
+                child: Text(
+                  "Verification Successful!! ",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 77, 89, 99),
+                    fontSize: 32,
+                    fontFamily: 'RobotoSlab-Bold',
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(
+                  top: 40,
+                  right: 15,
+                ),
+                child: Text(
+                  "Sustainable Solutions",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 50, 199, 55),
+                    fontSize: 30,
+                    fontFamily: 'RobotoSlab-Bold',
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(
+                  left: 5,
+                ),
+                child: Text(
+                  "Smart Waste Management: ",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 50, 199, 55),
+                    fontSize: 30,
+                    fontFamily: 'RobotoSlab-Bold',
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(
+                  right: 5,
+                ),
+                child: Text(
+                  "EcoBin Leads the Way!",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 50, 199, 55),
+                    fontSize: 30,
+                    fontFamily: 'RobotoSlab-Bold',
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 5, top: 30),
+                child: Container(
+                  height: 54,
+                  width: 370,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 14, 219, 21),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder:
+                              (context, animation, secondaryAnimation) =>
+                                  const LocationAddPage(),
+                          transitionsBuilder:
+                              (context, animation, secondaryAnimation, child) {
+                            const begin = Offset(1.0, 0.0);
+                            const end = Offset.zero;
+                            const curve = Curves.ease;
+
+                            var tween = Tween(begin: begin, end: end)
+                                .chain(CurveTween(curve: curve));
+
+                            return SlideTransition(
+                              position: animation.drive(tween),
+                              child: child,
+                            );
+                          },
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      "Get Started",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 26,
+                        fontFamily: 'RobotoSlab-Bold',
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, bottom: 10),
+                child: Image.asset(
+                  'assets/Group_308.jpg',
+                  width: 400.0,
+                  height: 330.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
