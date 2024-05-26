efd6e9ab8e2b8b89b3d1b2dc20e98fc568e7121e
Praveen Dilshan <rosapraveen1@gmail.com>
2024-05-26 21:43:34 +0530

    Add your location for detecting the garbage picks

---
+++ lib/Location_add_page.dart
@@ -0,0 +1,155 @@
+import 'package:eco_bin_original/Success.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // Importing Services Library for FilteringTextInputFormatter
+
+void main() {
+  runApp(LocationAddPage());
+}
+
+class LocationAddPage extends StatefulWidget {
+  const LocationAddPage({Key? key}) : super(key: key);
+
+  @override
+  _LocationAddPageState createState() => _LocationAddPageState();
+}
+
+class _LocationAddPageState extends State<LocationAddPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 5),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "Waste Drop off",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 50, 199, 55),
+                    fontSize: 28,
+                    fontFamily: 'RobotoSlab-Bold',
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "Here’s the part where you have to transport the waste to the dumpsite closest to you.",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  keyboardType:
+                      TextInputType.number, // Setting keyboardType to number
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly // Accept only digits
+                  ],
+                  decoration: InputDecoration(
+                    labelText: 'Your Address',
+                    hintText: 'Enter Your Address here',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: const BorderSide(
+                        color: Colors.green,
+                        width: 2,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: const BorderSide(
+                        color: Colors.green,
+                        width: 2,
+                      ),
+                    ),
+                    prefixIcon: const Icon(Icons.location_city),
+                    prefixIconColor: Colors.green,
+                    labelStyle: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Color.fromARGB(255, 3, 80, 5),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, top: 30),
+                child: Container(
+                  height: 54,
+                  width: 370,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 14, 219, 21),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder:
+                              (context, animation, secondaryAnimation) =>
+                                  const Success(),
+                          transitionsBuilder:
+                              (context, animation, secondaryAnimation, child) {
+                            const begin = Offset(1.0, 0.0);
+                            const end = Offset.zero;
+                            const curve = Curves.ease;
+
+                            var tween = Tween(begin: begin, end: end)
+                                .chain(CurveTween(curve: curve));
+
+                            return SlideTransition(
+                              position: animation.drive(tween),
+                              child: child,
+                            );
+                          },
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      "Proceed",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10, left: 10),
+                child: Image.asset(
+                  'assets/cuate.jpg',
+                  width: 400.0,
+                  height: 330.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
