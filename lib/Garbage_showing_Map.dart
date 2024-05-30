3bc354c4aca2077f7087b63d3774704f75af8d0e
Praveen Dilshan <rosapraveen1@gmail.com>
2024-05-30 09:55:23 +0530

    Adding garbage location

--- lib/Garbage_showing_Map.dart
+++ lib/Garbage_showing_Map.dart
@@ -8,6 +8,10 @@ void main() {
 class GarbageshowingMap extends StatelessWidget {
   static const LatLng _pGooglePlex =
       LatLng(7.1280940356164715, 79.88128287742241);
+  static const LatLng _GarbageLocation1 =
+      LatLng(7.131708950802051, 79.88039606539519);
+  static const LatLng _GarbageLocation2 =
+      LatLng(7.131612264487296, 79.87617271997918);
 
   const GarbageshowingMap({super.key});
 
@@ -39,8 +43,22 @@ class GarbageshowingMap extends StatelessWidget {
             },
           ),
         ),
-        body: const GoogleMap(
+        body: GoogleMap(
           initialCameraPosition: CameraPosition(target: _pGooglePlex, zoom: 13),
+          markers: {
+            Marker(
+              markerId: MarkerId("_garbageLocation1"),
+              icon: BitmapDescriptor.defaultMarkerWithHue(
+                  BitmapDescriptor.hueRed),
+              position: _GarbageLocation1,
+            ),
+            Marker(
+              markerId: MarkerId("_garbageLocation2"),
+              icon: BitmapDescriptor.defaultMarkerWithHue(
+                  BitmapDescriptor.hueRed),
+              position: _GarbageLocation2,
+            ),
+          },
         ),
       ),
     );
