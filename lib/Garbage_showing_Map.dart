6f4750dbf7a06190dcd750e6de347bc11eb3bb06
Praveen Dilshan <rosapraveen1@gmail.com>
2024-05-30 11:40:12 +0530

    Modified

--- lib/Garbage_showing_Map.dart
+++ lib/Garbage_showing_Map.dart
@@ -11,12 +11,40 @@ class GarbageshowingMap extends StatelessWidget {
   static const LatLng _GarbageLocation1 =
       LatLng(7.131708950802051, 79.88039606539519);
   static const LatLng _GarbageLocation2 =
-      LatLng(7.131612264487296, 79.87617271997918);
-
+      LatLng(7.131945323570431, 79.87612478326228);
+  static const LatLng _GarbageLocation3 =
+      LatLng(7.1284534794369705, 79.87694017478212);
   const GarbageshowingMap({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Set<Marker> markers = {
+      Marker(
+        markerId: MarkerId("_garbageLocation1"),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+        position: _GarbageLocation1,
+        onTap: () {
+          _showGarbageBinsInfo(context, 'Garbage Location 1');
+        },
+      ),
+      Marker(
+        markerId: MarkerId("_garbageLocation2"),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+        position: _GarbageLocation2,
+        onTap: () {
+          _showGarbageBinsInfo(context, 'Garbage Location 2');
+        },
+      ),
+      Marker(
+        markerId: MarkerId("_garbageLocation3"),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+        position: _GarbageLocation3,
+        onTap: () {
+          _showGarbageBinsInfo(context, 'Garbage Location 3');
+        },
+      ),
+    };
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Map',
@@ -45,21 +73,88 @@ class GarbageshowingMap extends StatelessWidget {
         ),
         body: GoogleMap(
           initialCameraPosition: CameraPosition(target: _pGooglePlex, zoom: 13),
-          markers: {
-            Marker(
-              markerId: MarkerId("_garbageLocation1"),
-              icon: BitmapDescriptor.defaultMarkerWithHue(
-                  BitmapDescriptor.hueRed),
-              position: _GarbageLocation1,
-            ),
-            Marker(
-              markerId: MarkerId("_garbageLocation2"),
-              icon: BitmapDescriptor.defaultMarkerWithHue(
-                  BitmapDescriptor.hueRed),
-              position: _GarbageLocation2,
-            ),
-          },
+          markers: markers,
         ),
+      ),
+    );
+  }
+
+  void _showGarbageBinsInfo(BuildContext context, String locationName) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: const EdgeInsets.all(16),
+          color: Color.fromARGB(255, 207, 230, 207),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                locationName,
+                style: const TextStyle(
+                  fontSize: 28,
+                  fontFamily: 'Jaro',
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const SizedBox(height: 8),
+              _buildGarbageBinInfo('Paper', 0.95, Colors.red),
+              _buildGarbageBinInfo('Glass', 0.50, Colors.blue),
+              _buildGarbageBinInfo('Organic', 0.25, Colors.green),
+              _buildGarbageBinInfo(
+                  'Plastic', 0.10, Color.fromARGB(255, 255, 230, 7)),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildGarbageBinInfo(String binName, double level, Color color) {
+    return ListTile(
+      leading: Icon(
+        Icons.delete,
+        color: color,
+        size: 35,
+      ),
+      title: Text(
+        binName,
+        style: const TextStyle(
+          fontSize: 20,
+          fontFamily: 'Roboto-Bold',
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+      subtitle: Row(
+        children: [
+          Expanded(
+            child: LinearProgressIndicator(
+              value: level,
+              backgroundColor: Color.fromARGB(255, 169, 172, 169),
+              color: color,
+            ),
+          ),
+          const SizedBox(width: 10),
+          Container(
+            width: 50,
+            height: 30,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              border:
+                  Border.all(color: Color.fromARGB(255, 0, 255, 17), width: 2),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Text(
+              '${(level * 100).toInt()}%',
+              style: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'Roboto-Bold',
+                color: Colors.black,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
